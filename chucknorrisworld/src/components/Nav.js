7759d79c8d3541290952475b4e8f7ea5268b5fe7
Alex Kiura <kiuraalex@gmail.com>
2017-02-27 09:40:06 +0300

    [Bug] export Nav component

--- chucknorrisworld/src/components/Nav.js
+++ chucknorrisworld/src/components/Nav.js
@@ -1,7 +1,7 @@
 import React , { Component } from 'react';
 import { Link } from 'react-router';
 
-class Nav extends Component {
+export default class Nav extends Component {
   render() {
     return(
       <nav className="navbar navbar-default">
