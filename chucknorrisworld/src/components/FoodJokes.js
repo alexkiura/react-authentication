bea4714b3368b49e3e6f2d8c9e3068e135de47a2
Alex Kiura <kiuraalex@gmail.com>
2017-03-11 13:58:06 +0300

    feat: render joke

--- chucknorrisworld/src/components/FoodJokes.js
+++ chucknorrisworld/src/components/FoodJokes.js
@@ -22,18 +22,23 @@ class FoodJokes extends Component {
   }
 
   render() {
-    const { jokes } = this.state
+    const {jokes} = this.state
     return (
       <div>
-        <Nav />
+        <Nav/>
         <h3 className="text-center">Chuck Norris food jokes</h3>
-        <hr/>
-
-        { jokes.map((joke, index) => (
+        <hr/> {jokes.map((joke, index) => (
           <div className="col-sm-6" key={index}>
-             
+            <div className="panel panel-primary">
+              <div className="panel-heading">
+                <h3 className="panel-title"><span className="btn"># { joke.id }</span></h3>
+              </div>
+              <div className="panel-body">
+                <p>{ joke.joke }</p>
+              </div>
+            </div>
           </div>
-        )) }
+        ))}
       </div>
     )
   }
