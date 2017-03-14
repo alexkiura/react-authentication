1db68cbd4213e3444538c4b09af19fffb376fb45
Alex Kiura <kiuraalex@gmail.com>
2017-03-14 14:51:34 +0300

    feat: add root component

--- chucknorrisworld/src/index.js
+++ chucknorrisworld/src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import CelebrityJokes from './components/CelebrityJokes';
+import FoodJokes from './components/FoodJokes';
+import { Router, Route, browserHistory } from 'react-router';
+
+
+const Root = () => {
+  return (
+    <div className="container">
+      <Router history={browserHistory}>
+        <Route path="/" component={ FoodJokes } />
+        <Route path="/special" component={ CelebrityJokes } />
+      </Router>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <App />,
+  <Root />,
   document.getElementById('root')
 );
