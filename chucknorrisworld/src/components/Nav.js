2c4f535489322211b14878b9168d99ad27c51148
Alex Kiura <kiuraalex@gmail.com>
2017-03-15 19:03:43 +0300

    chore: prettify login and logout buttons

--- chucknorrisworld/src/components/Nav.js
+++ chucknorrisworld/src/components/Nav.js
@@ -5,21 +5,23 @@ export default class Nav extends Component {
   render() {
     return(
       <nav className="navbar navbar-default">
-        <div className="navbar-header">
-          <Link className="navbra-brand" to="/">Chuck Norris World</Link>
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <Link className="navbar-brand" to="/">Chuck Norris World</Link>
+          </div>
+          <ul className="nav navbar-nav">
+            <li>
+              <Link to="/">Food Jokes</Link>
+            </li>
+            <li>
+              <Link to="/special">Celebrity Jokes</Link>
+            </li>
+          </ul>
+          <ul className="nav navbar-nav navbar-right">
+            <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <li><a href="#"><span className="glyphicon glyphicon-log-out"></span> Logout</a></li>
+          </ul>
         </div>
-        <ul className="nav navbar-nav">
-          <li>
-            <Link to="/">Food Jokes</Link>
-          </li>
-          <li>
-            <Link to="/special">Celebrity Jokes</Link>
-          </li>
-        </ul>
-        <ul className="nav navbar-nav navbar-right">
-          <li><button className="btn btn-info log">Log In</button></li>
-          <li><button className="btn btn-danger log">Log Out</button></li>
-        </ul>
       </nav>
     )
   }    
